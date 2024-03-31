28677eca98c30a35a847024d449670757df0d0cd
De Gitgit Agitya <degitgitagitya@gmail.com>
2024-03-31 11:45:44 +0700

    style(eslint): fix eslint warning

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -11,22 +11,21 @@ export default async function handler(
     const jsonResponse = await handleUpload({
       body,
       request,
-      onBeforeGenerateToken: async (
-        pathname
+      onBeforeGenerateToken: async () =>
         /* clientPayload */
-      ) => {
-        // Generate a client token for the browser to upload the file
-        // ⚠️ Authenticate and authorize users before generating the token.
-        // Otherwise, you're allowing anonymous uploads.
+        {
+          // Generate a client token for the browser to upload the file
+          // ⚠️ Authenticate and authorize users before generating the token.
+          // Otherwise, you're allowing anonymous uploads.
 
-        return {
-          allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
-          tokenPayload: JSON.stringify({
-            // optional, sent to your server on upload completion
-            // you could pass a user id from auth, or a value from clientPayload
-          })
-        }
-      },
+          return {
+            allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
+            tokenPayload: JSON.stringify({
+              // optional, sent to your server on upload completion
+              // you could pass a user id from auth, or a value from clientPayload
+            })
+          }
+        },
       onUploadCompleted: async ({ blob, tokenPayload }) => {
         // Get notified of client upload completion
         // ⚠️ This will not work on `localhost` websites,
